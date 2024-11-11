a90d718a7d2932ee7d904e099fdd478cae8eb6aa
Gabriel Braga <gabrielbraga@mini-de-gabriel.lan>
2024-11-11 00:45:10 -0300

    Iterador adicionado

--- olhoVivo_linhas.py
+++ olhoVivo_linhas.py
@@ -34,6 +34,18 @@ letreiros_encontrados = []
 
 #iterar sobre os numeros 1 a 89
 for i in range(1,99):
-    
+    linhas_url = f"{url_base}/Linha/Buscar?termoBusca={i}"
+    linhas_response = requests.get(linhas_url)
+    linhas = linhas_response.json()
 
-# %%
+    #adicionar letreiros encontrados à lista
+    if isinstance(linhas, list):
+        for linha in linhas:
+            letreiro = linha['lt']
+            if letreiro not in letreiros_encontrados:
+                letreiros_encontrados.append(letreiro)
+    else:
+        print(f"Resposta inesperada para o termo de busca {i}: {linhas}")
+    
+for letreiro in letreiros_encontrados:
+    print(letreiro)
